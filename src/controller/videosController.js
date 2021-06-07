a412d6782aaf523bd8530edeb8ab5f89506a4991
Deployer <drivespublic@gmail.com>
2021-06-07 16:46:55 +0900

    note PC

--- src/controller/videosController.js
+++ src/controller/videosController.js
@@ -1,11 +1,43 @@
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createAt: "2 minutes ago",
+    views: 1,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
+
 export const home = (req, res) => {
-  res.render("home", { pageTitle: "Home" });
+  return res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => {
-  res.render("watch", { pageTitle: "Watch" });
+export const watch = (req, res) => {
+  const {
+    params: { id },
+  } = req;
+
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
 };
 export const edit = (req, res) => {
-  res.render("edit", { pageTitle: "Edit Video" });
+  return res.render("edit", { pageTitle: "Edit Video" });
 };
 export const search = (req, res) => res.send("Search");
 export const deleteVideo = (req, res) => res.send("delete video");
